1f9df00b576120d33772fb05ddd18cc9b173a54f
Ali Eren <46861482+Allie198@users.noreply.github.com>
2025-11-28 15:53:06 +0300

    Blockchain sınıfı eklendi

--- blockchain.js
+++ blockchain.js
@@ -62,3 +62,72 @@ export class Block {
         return true;
     }
 }
+
+
+export class Blockchain {
+    constructor (difficulty=2, miningReward=100, filename="data.json") {
+        this.chain = [this.createGenesisBlock()];
+        
+        this.difficulty = difficulty;
+        this.miningReward = miningReward;
+        this.filename = filename;
+        this.pendingTransactions = [];
+
+        this.load();
+    }
+
+    save() {
+        fs.writeFileSync(this.filename, JSON.stringify(this.chain,null, 4))
+    }
+
+    load() {
+        if (fs.existsSync(this.filename)) {
+            const data = JSON.parse(fs.readFileSync(this.filename));
+            this.chain = data.map(
+                b => new Block(b.idx, b.timestamp, b.transactions, b.prevHash)
+            );
+        }
+    }
+
+    createGenesisBlock() {
+            return new Block(0, new Date().toISOString(), [], "0")
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addTransaction(transaction) { 
+        if (!transaction.isValid()) throw new Error("Geçersiz transaction");
+        this.pendingTransactions.push(transaction);
+    }
+
+    minePendingTransactions(minerAddress) {
+        const block = new Block (
+            this.chain.length, new Date().toISOString(), this.pendingTransactions, this.getLatestBlock().hash
+        );
+
+        block.mineBlock(this.difficulty);
+
+        console.log("Block mine edildi");
+        
+        this.chain.push(block);
+        this.pendingTransactions = [new Transaction(null, minerAddress, this.miningReward)];
+        
+        this.save();
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const curr = this.chain[i];
+            const prev = this.chain[i - 1];
+
+            if (!curr.hasValidTransactions()) return false;
+            if (curr.hash !== curr.calculateHash()) return false;
+            if (curr.prevHash !== prev.hash) return false;
+        }
+
+        return true;
+    }
+}
+
