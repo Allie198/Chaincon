fdbe85286d688d4b5a61515bbeadf1fd73aeadeb
Ali Eren <46861482+Allie198@users.noreply.github.com>
2025-11-28 23:26:38 +0300

    Signature eklendi

--- transactions.js
+++ transactions.js
@@ -6,7 +6,7 @@ export default class Transaction {
     }
 
     isValid() {
-        // Mining reward transaction: from === null
+ 
         if (this.from === null) return true;
 
         if (!this.from || !this.to) return false;
